582c4dbdfff9b6fc07676e15204a4bb6599de66f
Frank Daniel <mbuthiawanganju@gmail.com>
2025-09-11 14:19:48 +0300

    Stiill adjusting

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -3,7 +3,7 @@ from relationship_app.models import Author, Book, Library, Librarian
 # Query all books by a specific author
 author_name = "J.K. Rowling"
 author = Author.objects.get(name=author_name)
-books_by_author = author.book_set.all()
+books_by_author = Book.objects.filter(author=author)
 
 # List all books in a library
 library_name = "Central Library"
@@ -12,5 +12,5 @@ books_in_library = library.books.all()
 
 # Retrieve the librarian for a library
 librarian = Librarian.objects.get(library=library)
-# or simply
-librarian = library.librarian
+# or equivalently:
+# librarian = library.librarian
