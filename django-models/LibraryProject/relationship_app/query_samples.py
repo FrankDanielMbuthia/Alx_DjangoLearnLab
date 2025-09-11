138d7329e9efea4ace57ba6062a6b095951d8ef0
Frank Daniel <mbuthiawanganju@gmail.com>
2025-09-11 14:18:10 +0300

    Still adjusting to satisfy the checker

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,8 +1,16 @@
-author = Author.objects.get(name="J.K. Rowling")  
-books = author.book_set.all()   # default reverse lookup
+from relationship_app.models import Author, Book, Library, Librarian
 
-library = Library.objects.get(name="Central Library")
-books = library.books.all()   # because it's a ManyToMany
+# Query all books by a specific author
+author_name = "J.K. Rowling"
+author = Author.objects.get(name=author_name)
+books_by_author = author.book_set.all()
 
+# List all books in a library
+library_name = "Central Library"
+library = Library.objects.get(name=library_name)
+books_in_library = library.books.all()
+
+# Retrieve the librarian for a library
 librarian = Librarian.objects.get(library=library)
-librarian = library.librarian   # Django auto creates this attribute
+# or simply
+librarian = library.librarian
