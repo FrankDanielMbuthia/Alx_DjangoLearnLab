a5a8ed0c08ab37a8b0506eb124734d44afceae88
Frank Daniel <mbuthiawanganju@gmail.com>
2025-09-15 11:04:36 +0300

    Practiced user authentication basics

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
 from django.views.generic.detail import DetailView
 from .models import Book
 from .models import Library
+from django.contrib.auth import login
 
 def list_books(request):
     books = Book.objects.all()
@@ -16,3 +18,15 @@ class LibraryDetailView(DetailView):
         # Prefetch the related books and their authors to reduce DB hits.
         # This assumes Library -> Book related_name is 'books' and Book has FK 'author'.
         return Library.objects.prefetch_related('books__author')
+    
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()  # create the user
+            login(request, user)  # log the user in after registration
+            return redirect("home")  # adjust if your homepage URL name is different
+    else:
+        form = UserCreationForm()
+    
+    return render(request, "relationship_app/register.html", {"form": form})
