96cd63ea3a067d209b987cba2952d392d911b5e7
Frank Daniel <mbuthiawanganju@gmail.com>
2025-09-15 19:50:33 +0300

    Learnt on role based access

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -4,6 +4,7 @@ from django.views.generic.detail import DetailView
 from .models import Book
 from .models import Library
 from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required, user_passes_test
 
 def list_books(request):
     books = Book.objects.all()
@@ -30,3 +31,24 @@ def register(request):
         form = UserCreationForm()
     
     return render(request, "relationship_app/register.html", {"form": form})
+
+def is_admin(user):
+    return user.userprofile.role == "admin"
+
+def is_librarian(user):
+    return user.userprofile.role == 'Librarian'
+
+def is_member(user):
+    return user.userprofile.role == 'Member'
+
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, "admin_view.html")
+
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, "librarian_view.html")
+
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, "member_view.html")
