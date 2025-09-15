841c6a19ee3e45de1667752567c5dc561fd5e934
Frank Daniel <mbuthiawanganju@gmail.com>
2025-09-15 19:54:25 +0300

    Adjusted role based exercise task

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -32,7 +32,7 @@ class UserProfile(models.Model):
 
     ROLE_CHOICES = [
         ('Admin', 'Admin'),
-        ('Librarian', 'Librarian'),
+        ('Librarians', 'Librarians'),
         ('Member', 'Member'),
     ]
     role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='Member')
